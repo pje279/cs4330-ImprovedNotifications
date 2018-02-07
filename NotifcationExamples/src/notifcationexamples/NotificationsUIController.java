7dcf51b1ce4f81a02a6210c166e617057506cf57
Peter <p.eckelmann@gmail.com>
2018-02-07 10:23:43 -0600

    Appears to work appropriately, need to ask professor if approach is okay

--- NotifcationExamples/src/notifcationexamples/NotificationsUIController.java
+++ NotifcationExamples/src/notifcationexamples/NotificationsUIController.java
@@ -13,6 +13,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -25,8 +26,22 @@ import taskers.*;
  */
 public class NotificationsUIController implements Initializable, Notifiable {
 
+    public enum State { NULL, STOPPED, STARTED};
+    
     @FXML
     private TextArea textArea;
+    
+    @FXML
+    private Button btnTask1;
+    private State task1State = State.NULL;
+    
+    @FXML
+    private Button btnTask2;
+    private State task2State = State.NULL;
+    
+    @FXML
+    private Button btnTask3;
+    private State task3State = State.NULL;
     
     private Task1 task1;
     private Task2 task2;
@@ -50,11 +65,18 @@ public class NotificationsUIController implements Initializable, Notifiable {
     @FXML
     public void startTask1(ActionEvent event) {
         System.out.println("start task 1");
-        if (task1 == null) {
+        if (task1 == null || this.task1State == State.NULL || this.task1State == State.STOPPED) {
             task1 = new Task1(2147483647, 1000000);
             task1.setNotificationTarget(this);
             task1.start();
+            this.btnTask1.setText("End Task 1");
+            this.task1State = State.STARTED;
         }
+        else if (this.task1 != null && this.task1State == State.STARTED)
+        {
+            task1.end();
+            this.btnTask1.setText("Start Task 1");
+            this.task1State = State.STOPPED;
     }
     
     @Override
@@ -68,20 +90,27 @@ public class NotificationsUIController implements Initializable, Notifiable {
     @FXML
     public void startTask2(ActionEvent event) {
         System.out.println("start task 2");
-        if (task2 == null) {
+        if (task2 == null || this.task2State == State.NULL || this.task2State == State.STOPPED) {
             task2 = new Task2(2147483647, 1000000);
             task2.setOnNotification((String message) -> {
                 textArea.appendText(message + "\n");
             });
             
             task2.start();
+            this.btnTask2.setText("End Task 2");
+            this.task2State = State.STARTED;
         }        
+        else if (this.task2 != null && this.task2State == State.STARTED)
+        {
+            task2.end();
+            this.btnTask2.setText("Start Task 2");
+            this.task2State = State.STOPPED;
     }
     
     @FXML
     public void startTask3(ActionEvent event) {
         System.out.println("start task 3");
-        if (task3 == null) {
+        if (task3 == null || this.task3State == State.NULL || this.task3State == State.STOPPED) {
             task3 = new Task3(2147483647, 1000000);
             // this uses a property change listener to get messages
             task3.addPropertyChangeListener((PropertyChangeEvent evt) -> {
@@ -89,6 +118,13 @@ public class NotificationsUIController implements Initializable, Notifiable {
             });
             
             task3.start();
+            this.btnTask3.setText("End Task 3");
+            this.task3State = State.STARTED;
         }
+        else if (this.task3 != null && this.task3State == State.STARTED)
+        {
+            task3.end();
+            this.btnTask3.setText("Start Task 3");
+            this.task3State = State.STOPPED;
     } 
 }
